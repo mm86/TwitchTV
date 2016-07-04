222b34a225d794020ca0dc1f0a6458352f4b6d40
mm86 <madhuvanmahesh@gmail.com>
2016-07-04 14:08:39 -0700

    feat: Connect to TwitchTV API and retrieve data
    Connect to TwitchTV API through getJSON and retrieve data such as name, picture, status etc.

---
+++ app.js
@@ -0,0 +1,23 @@
+(function() {
+
+  var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","brunofin","comster404","test_channel","cretetion","sheevergaming","TR7K","OgamingSC2","ESL_SC2"];
+    
+    $.getJSON('https://api.twitch.tv/kraken/streams/brunofin?callback=?', function(data) {
+      //need picture,name with twicth channel url, status message, description
+      if(data.stream===null){
+        console.log(data.stream);
+      }
+      else if(data.status === 422){
+        console.log(data.status);
+      }
+      else {
+        console.log(data);
+      }
+        //console.log(data.stream.channel.display_name);
+        //console.log(data.stream.channel.profile_banner);
+        //console.log(data.stream.channel.game);
+
+    });
+
+
+})();
