ed38ba34c62de799aa2fdc0572df0a1e5a407224
mm86 <madhuvanmahesh@gmail.com>
2016-07-04 20:01:34 -0700

    fix: Check if channel is streaming, offline or removed

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 
   var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","brunofin","comster404","test_channel","cretetion","sheevergaming","TR7K","OgamingSC2","ESL_SC2"];
     
-    $.getJSON('https://api.twitch.tv/kraken/streams/brunofin?callback=?', function(data) {
+    $.getJSON('https://api.twitch.tv/kraken/streams/storbeck?callback=?', function(data) {
       //need picture,name with twicth channel url, status message, description
       if(data.stream===null){
-        console.log(data.stream);
+        console.log(data);
+         $.getJSON('https://api.twitch.tv/kraken/channels/storbeck?callback=?', function(data) {
+            console.log(data);
+         });
       }
       else if(data.status === 422){
         console.log(data.status);
