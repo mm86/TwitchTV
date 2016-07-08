6dc172804e85c34559a1eee4b78da2f010b18e17
mm86 <madhuvanmahesh@gmail.com>
2016-07-07 21:17:11 -0700

    fix: Use closure to access channels array inside getjson request

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
+//solved closure and callback classic issue with the help of this link http://stackoverflow.com/questions/15347750/getjson-and-for-loop-issue
 $(function() {
 
-  var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx","RobotCaleb","thomasballinger","noobs2ninjas","beohoff","brunofin","comster404","test_channel","cretetion","sheevergaming","TR7K","OgamingSC2","ESL_SC2"];
-    
-    $.getJSON('https://api.twitch.tv/kraken/streams/storbeck?callback=?', function(data) {
-      //need picture,name with twicth channel url, status message, description
-      if(data.stream===null){
-        console.log(data);
-         $.getJSON('https://api.twitch.tv/kraken/channels/storbeck?callback=?', function(data) {
-            console.log(data);
-         });
-      }
-      else if(data.status === 422){
-        console.log(data.status);
-      }
-      else {
-        console.log(data);
-      }
-        //console.log(data.stream.channel.display_name);
-        //console.log(data.stream.channel.profile_banner);
-        //console.log(data.stream.channel.game);
-
-    });
+    var channels = ["freecodecamp", "storbeck", "terakilobyte", "habathcx", "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "brunofin", "comster404", "test_channel", "cretetion", "sheevergaming", "TR7K", "OgamingSC2", "ESL_SC2"];
 
 
-})();
+    for (var i=0;i<channels.length;i++) {
+        (function(i) {
+        $.getJSON('https://api.twitch.tv/kraken/streams/'+channels[i]+'?callback=?', function(data) {
+            if (data.stream === null) {             
+                $.getJSON(data._links.channel+'?callback=?', function(data) {
+                  console.log(data.logo);
+                  $('#channels ul').append('<li><img src="'+data.logo+'"/><p><a href="'+data.url+'">'+data.display_name+'</a></p><p>Offline</p>');
+                });
+            } else if (data.status === 422) {
+                $("#channels ul").append('<li>'+channels[i]+', account closed</li>'); 
+          
+            } else {
+                $('#channels ul').append('<li><img src='+data.stream.channel.logo+'/><p><a href="'+data.stream.channel.url+'">'+data.stream.channel.display_name+'</a></p><p>'+data.stream.channel.status+'</p>');
+
+            }
+
+        });
+    })(i);
+
+}
+
+});
